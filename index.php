a62f2575cea7f5d039430bacfa6e99041551f206
Espen Raugstad <espen.raugstad@gmail.com>
2024-09-08 16:02:14 +0200

    Satt opp landing, login og dashboard

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/global.css">
+    <title>Cognitio</title>
+</head>
+<body>
+    Hello Cognitio!
+    <img src="./img/cognitio_logo_sort.png" alt="" height="25">
+
+    <a href="./login.php">Logg inn</a>
+    <a href="">Registrer</a>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/global.css">
+    <title>Logg inn</title>
+</head>
+
+<body>
+<img src="./img/cognitio_logo_sort.png" alt="" height="25">
+    <p>Logg inn på din konto.</p>
+    <form action="">
+        <div>
+            <label for="username">Brukernavn</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="password">Passord</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <input type="submit" value="Logg inn">
+        </div>
+    </form>
+    <p>
+        <a href="./dashboard.php">Gå til dashboard</a>
+    </p>
+    <p>
+        <a href="">
+            Ingen bruker? Registrer deg her &rarr;
+        </a>
+    </p>
+</body>
+
+</html>
