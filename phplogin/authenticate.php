dd5f61d475f4c3d12ea6790bdc6f94008fe6d313
liahra <chrira17@uia.no>
2024-10-17 12:34:30 +0200

    Lagt til brukerprofil.

--- phplogin/authenticate.php
+++ phplogin/authenticate.php
@@ -5,27 +5,27 @@
     $DATABASE_PASS = 'root';
     $DATABASE_NAME = 'phplogin';
 
-    // Koble til databasen med angitt informasjon.
+    // Koble til databasen.
     $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
     if ( mysqli_connect_errno() ) {
-        // // Hvis feil med tilkoblig til databasen.
+        // // Hvis feil.
         exit('Mislyktes å koble til MySQL: ' . mysqli_connect_error());
     } else {
         echo 'Vellykket tilkobling til MySQL!<br>';
     }
 
-    // / Har form blitt sendt?
+    // / Form sendt?
     if ( !isset($_POST['username'], $_POST['password']) ) {
-        // Kan ikke hente data som skulle blitt sendt
+        // Kan ikke hente data.
         exit('<br><b> Vennligst fyll inn feltene for både brukernavn og passord! </b>');
     }
 
-    // Forbereder SQL-spørringene -> forhindrer SQL-injections.
+    // Forbereder SQL-spørringene.
     if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
         // Bind parameter (s = string)
         $stmt->bind_param('s', $_POST['username']); // Brukerinput håndteres som data og ikke som en del av selve SQL-kommandoen.
         $stmt->execute();
-        $stmt->store_result(); // Lagre resultatet så kontoen kan sjekkes i databasen.
+        $stmt->store_result(); // Lagre resultatet.
 
         if ($stmt->num_rows > 0) {
             $stmt->bind_result($id, $password);
@@ -33,8 +33,8 @@
 
             // Konto eksisterer, verifiser passord.
             if ($_POST['password'] === $password) {
-            // Bruker er verifisert og har logget inn.
-            // Lag 'sessions' for å vite at brukeren er logget inn.
+            // Bruker verifisert og logget inn.
+            // Lag tilhørende bruker-sessions.
                 $_SESSION['loggedin'] = TRUE;
                 $_SESSION['name'] = $_POST['username'];
                 $_SESSION['id'] = $id;
---
+++ phplogin/profile.php
@@ -0,0 +1,70 @@
+<?php
+// Start bruker-session.
+session_start();
+
+// Er bruker innlogget?
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit;
+}
+
+// Db-tilkobling
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'root';
+$DATABASE_NAME = 'phplogin';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+if (mysqli_connect_errno()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Hent passord og epost fra db.
+$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+// Hent brukerinfo fra ID
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email);
+$stmt->fetch();
+$stmt->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Profil</title>
+</head>
+<body class="loggedin">
+    <div class="navtop">
+        <div>
+            <h1>Cognitio</h1>
+			<a href="profile.php"><i class="fas fa-user-circle"></i>Profil</a>
+			<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logg ut</a>
+        </div>
+    </div>
+    <div class="content">
+        <h2>Brukerprofil</h2>
+        <p>Kontoinformasjon:</p>
+        <div>
+            <table>
+                <tr>
+                    <td>Brukernavn:</td>
+                    <td><?=htmlspecialchars($_SESSION['name'], ENT_QUOTES)?></td>
+                </tr>
+                <tr>
+                    <td>Passord:</td>
+                    <td><?=htmlspecialchars($password, ENT_QUOTES)?></td>
+                </tr>
+                <tr>
+                    <td>E-post:</td>
+                    <td><?=htmlspecialchars($email, ENT_QUOTES)?></td>
+                </tr>
+            </table>
+        </div>
+    </div>   
+</body>
+</html>
