703a72c7510707eaa044b1c649437a24450ac73b
liahra <chrira17@uia.no>
2024-10-19 13:16:49 +0200

    Lagt til navbar. Må integreres og fikse hidden span on close.

---
+++ navbar/app.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSidebar() {
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+}
