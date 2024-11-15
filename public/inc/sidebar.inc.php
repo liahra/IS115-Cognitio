2e09ed3ab5b82fc66323106de7efddbd5114e572
espenraugstad <espen.raugstad@gmail.com>
2024-11-15 10:48:04 +0100

    Oppdaterte linken til scriptet i sidebar

--- public/inc/sidebar.inc.php
+++ public/inc/sidebar.inc.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <script src="./resources/js/app.js" defer></script>
+    <script src="../resources/js/app.js" defer></script>
 
     <title>Sidebar</title>
 </head>
