1ff66e99e03673f5b9ba6f7ca6cdd9a016dd160d
espenraugstad <espen.raugstad@gmail.com>
2024-11-15 21:45:44 +0100

    Oppdaterte logout ikonet

--- public/inc/sidebar.inc.php
+++ public/inc/sidebar.inc.php
@@ -39,7 +39,7 @@
             </li>
             <li>
                 <a href="logout.php">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
                     <span>Logg ut</span>
                 </a>
             </li>
