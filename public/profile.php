757a10e45f57ce668ff55d7bb72b4dc079f12642
espenraugstad <espen.raugstad@gmail.com>
2024-11-29 23:04:03 +0100

    La til klassen li på li-elementer på profilsiden for å unngå at stylingen kolliderer med sidebar

--- public/profile.php
+++ public/profile.php
@@ -28,11 +28,11 @@ $account = unserialize($_SESSION['account']);
             align-items: start;
         }
 
-        li:not(:first-child) {
+        .li:not(:first-child) {
             margin: 0.5rem 0;
         }
 
-        li {
+        .li {
             display: flex;
             align-items: center;
 
@@ -122,17 +122,17 @@ $account = unserialize($_SESSION['account']);
                             } ?> alt="" class="profile_picture">
             </div>
             <ul class="no-style">
-                <li>
+                <li class="li">
 
                     <h3><?= htmlspecialchars($account->getFirstName() . " " . $account->getLastName(), ENT_QUOTES) ?></h3>
                 </li>
-                <li>
+                <li class="li">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
                         <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
                     </svg>
                     <?= htmlspecialchars($account->getUsername(), ENT_QUOTES) ?>
                 </li>
-                <li>
+                <li class="li">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
                         <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" />
                     </svg> <?= htmlspecialchars($account->getEmail(), ENT_QUOTES) ?>
