2457309652a66facb33d86d107ddd4a67229392e
espenraugstad <espen.raugstad@gmail.com>
2024-12-01 20:54:11 +0100

    La til tag som markerer oppgaver som seine hvis dato har passert og de fremdeles ikke er fullført

--- public/home.php
+++ public/home.php
@@ -11,6 +11,7 @@ require_once '../src/account.php';
 
 $account = unserialize($_SESSION['account']);
 $tasks = $account->getUpcomingTasks(); // Henter oppgaver for den spesifikke brukeren
+
 $todos = $account->getUnfinishedTodos(); // Henter gjøremål for den spesifikke brukeren
 
 require "../src/inc/utilities.inc.php";
@@ -26,6 +27,23 @@ require "../src/inc/utilities.inc.php";
     <link rel="stylesheet" href="./resources/css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
     <link rel="stylesheet" href="./resources/css/home.css">
+
+    <style>
+        .ongoing, .late{
+            width: fit-content;
+            padding: 0.25rem .5rem;
+            border-radius: 10000px;
+            font-size: 0.85rem;
+        }
+        .ongoing{
+            background-color: lightgreen;
+            color: white;
+        }
+        .late{
+            background-color: red;
+            color: white;
+        }
+    </style>
 </head>
 
 
@@ -58,6 +76,7 @@ require "../src/inc/utilities.inc.php";
                                     📝 <?= htmlspecialchars($task['title'], ENT_QUOTES) ?>
                                 </a>
                             </h3>
+                            <?php echo isLate($task['due_date'], $task['status']) ? "<div class='late'>Sein</div>" : ""; ?>
                             <div class="task-details">
                                 <div><strong>Emne:</strong> <?= htmlspecialchars($task['course_code'], ENT_QUOTES) ?></div>
                                 <span class="separator">|</span>
